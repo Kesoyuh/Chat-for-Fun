81b240677f56ff31a230ea25b4b05b2c5087ce61
Changchang Wang <Jeffreywang1943@gmail.com>
2016-08-22 18:38:56 +1000

    initial commit

---
+++ Chat-for-Fun/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chat-for-Fun
+//
+//  Created by Jeffrey on 22/08/2016.
+//  Copyright © 2016 University of Melbourne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
